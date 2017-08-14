e1d795acbe23fecb38dd060e340ad5fdbe1f0d08
Adam Wead <amsterdamos@gmail.com>
2017-08-14 15:16:05 -0400

    Use IndexingAdapter to index multiple items
    Copied from samvera-labs/valkyrie, this is an adapter that will
    simultaneously write to a persister and solr at the same time.
    
    Writes to solr are done in bulk to save indexing time.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,4 +4,9 @@ ENV['RACK_ENV'] = 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 
-# @todo A more thorough RSpec configuration
+RSpec.configure do |config|
+  config.before(:each) do
+    Blacklight.default_index.connection.delete_by_query('*:*')
+    Blacklight.default_index.connection.commit
+  end
+end
