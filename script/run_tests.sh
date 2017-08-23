279eade5b046e77c7651a665821216151e44f3c1
Adam Wead <amsterdamos@gmail.com>
2017-08-23 14:22:38 -0400

    Updating README with report results and methods

--- script/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bundle exec rake testing_support:clean
-bundle exec rake active_fedora_testing:all
-bundle exec rake testing_support:clean
-bundle exec rake fedora_testing:all
-bundle exec rake testing_support:clean
-bundle exec rake postgres_testing:all
